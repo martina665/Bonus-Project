a98fa64d64788c5ea7cdbf5f721ba04c5514e451
giudm <giuliadimartino.2203@gmail.com>
2022-01-26 15:12:51 +0100

    modifica

--- unsupervised_sentiment_analysis.R
+++ unsupervised_sentiment_analysis.R
@@ -17,3 +17,25 @@ str(DictionaryHE)
 
 data(DictionaryGI)  
 str(DictionaryGI)
+
+#4.CREATE A CORPUS
+corpus_sentiment <- VCorpus(VectorSource(data1$course_feedback)) 
+
+#5.SENTIMENT ANALYSIS WITH 'ANALYZE-SENTIMENT'
+# Within this function directly perform the pre-processing steps.
+system.time(sentiment <- analyzeSentiment(corpus_sentiment, #the object of class corpus
+                                          language = "english", 
+                                          removeStopwords = TRUE, 
+                                          stemming = TRUE, 
+                                          removePunctuation = T,
+                                          removeNumbers = T,
+                                          tolower = T))
+sentiment
+head(sentiment) 
+
+head(sentiment$SentimentGI)
+head(sentiment$SentimentHE)
+head(sentiment$SentimentLM)
+
+#6.View sentiment direction
+convertToDirection(sentiment$SentimentGI)
